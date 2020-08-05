53cb744f67bcc22b483a00e565bf500b9e49df10
ucchy84 <k208576@ie.u-ryukyu.ac.jp>
2020-08-06 00:23:33 +0900

    親コンポーネントから子コンポーネントへ

---
+++ hello_app/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <HelloWorld v-bind:title="message"/>
+    <hr>
+    <button v-on:click="doAction">Change title</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data:function() {
+    return {
+      message: 'HELLO',
+    };
+  },
+  methods: {
+    doAction:function() {
+      var input = prompt("new title:");
+      this.message = input;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ hello_app/src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>{{title}}</h1>
+    <p>{{message}}</p>
+    <hr>
+    <div>
+      <input type="text" v-model="input">
+      <button v-on:click="doAction">Click</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    title: String,
+  },
+
+  data:function() {
+    return {
+      message: 'お名前は？',
+      input: 'no name',
+    };
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  margin:0px;
+  padding:opx;
+  text-align:left;
+}
+h1 {
+  font-size:72px;
+  font-weight:bold;
+  text-align:right;
+  letter-spacing:-8px;
+  color:#f0f0f0;
+  margin:0px;
+}
+p {
+  margin:0px;
+  color:#666;
+  font-size:16px;
+}
+</style>
